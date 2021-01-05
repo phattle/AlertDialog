7afa79a730ad2d0ac3de3d6007283e17763571ae
Phát Lê <lethanhphat1408@gmail.com>
2021-01-05 18:15:12 +0700

    Dialog thông báo message

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +21,35 @@ public class MainActivity extends AppCompatActivity {
         mBtnShowDialog = findViewById(R.id.buttonShowDialog);
 
         //Design pattern java : 22 design pattern
+//        Order order = new Order.Builder()
+//                .setOrderType(OrderType.ON_SITE)
+//                .build();
+//
+//        Log.d("BBB",order.getOrderType() + "");
+        //Design pattern android: mvv, mvi, mvc, mvp, architecture
 
-        //Design pattern android: mvvm, vi, mvc, mvp, architecture
+        mBtnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this)
+                                                        .setTitle("Thông báo").setMessage("Phiên bản mới cần được cập nhật !!!").setIcon(R.mipmap.ic_launcher).setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "có", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        }).setCancelable(false);
+                        alertDialog.show();
+            }
+        });
     }
 }
